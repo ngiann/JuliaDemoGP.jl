2d21cda27a99a593745cbb0a641ab54e5033551b
Nikos Gianniotis <ngiann@gmail.com>
2024-11-08 09:50:09 +0100

    add pkgs only to register pkg

--- src/JuliaDemoGP.jl
+++ src/JuliaDemoGP.jl
@@ -1,12 +1,12 @@
 module JuliaDemoGP
 
-    using LinearAlgebra, Distances, Distributions
+    using LinearAlgebra, Distances, Distributions, RecipesBase  
 
     include("kernels.jl") # rbf and ou kernel
 
     include("gp.jl") # vanilla gp implementation
 
-    # include("plotextension.jl") # allows use of Plot.jl on own gp implementation
+    include("plotextension.jl") # allows use of Plot.jl on own gp implementation
 
     include("toydata.jl") # synthetic data for demonstrating gp fitting
 
