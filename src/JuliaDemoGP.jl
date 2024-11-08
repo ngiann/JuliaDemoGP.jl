7d2911cadb17bd99e3f19b37c758e190f453ac5e
Nikos Gianniotis <ngiann@gmail.com>
2024-11-08 09:37:56 +0100

    minor

--- src/JuliaDemoGP.jl
+++ src/JuliaDemoGP.jl
@@ -1,13 +1,15 @@
 module JuliaDemoGP
 
-    using LinearAlgebra
+    using LinearAlgebra, Distances, Distributions
 
     include("kernels.jl") # rbf and ou kernel
 
     include("gp.jl") # vanilla gp implementation
 
-    include("plotextension.jl") # allows use of Plot.jl on own gp implementation
+    # include("plotextension.jl") # allows use of Plot.jl on own gp implementation
 
     include("toydata.jl") # synthetic data for demonstrating gp fitting
+
+    export RBF, evaluatekernel, numparams, calculatecovariance
 
 end
